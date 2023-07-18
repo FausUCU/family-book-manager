d99dba33591e456a2d371a382b1b8f09544907be
FausUCU <88690038+FausUCU@users.noreply.github.com>
2023-07-18 19:38:06 -0300

    json manger working
    Json manager working, needs to found way of gating relativa path in java maeven

--- bibliotecaFamiliar/src/main/java/com/mycompany/bibliotecafamiliar/BibliotecaFamiliar.java
+++ bibliotecaFamiliar/src/main/java/com/mycompany/bibliotecafamiliar/BibliotecaFamiliar.java
@@ -24,43 +24,23 @@ import java.net.URISyntaxException;
 public class BibliotecaFamiliar {
 
     public static void main(String[] args) {
-        
-        /*
-        String mainClassRelativePath = getMainClassRelativePath();
 
-        System.out.println("Relative path of the main class: " + mainClassRelativePath);
-        
-        */
-       
-        //========================
-        /*
+    
         String filePath = "C:\\Users\\faust\\Desktop\\Java\\family-book-manager\\bibliotecaFamiliar\\target\\classes\\com\\mycompany\\bibliotecafamiliar\\libros.json";
 
         JSONFileManager jsonFileManager = new JSONFileManager();
         List<JSONObject> jsonList = jsonFileManager.readListOfJsonsFromFile(filePath);
 
+        String first=jsonList.get(0).toJSONString();
+        System.out.println(first);
+
+        /* 
         // Display the content of the JSON list
         for (JSONObject json : jsonList) {
             System.out.println(json.toJSONString());
         }
-        
         */
-        
-        //==============================00
-        
-       
-       //String filePath ="C:\\Users\\faust\\Desktop\\Java\\family-book-manager\\bibliotecaFamiliar\\src\\main\\java\\com\\mycompany\\bibliotecafamiliar\\libros.json";
-      //String filePath ="C:\\Users\\faust\\Desktop\\Java\\family-book-manager\\bibliotecaFamiliar\\target\\classes\\com\\mycompany\\bibliotecafamiliar\\libros.json";
-       String filePath ="com\\mycompany\\bibliotecafamiliar\\libros.json";
-       JSONFileManager jsonFileManager = new JSONFileManager();
-       List<JSONObject> jsonList = jsonFileManager.readListOfJsonsFromFile(filePath);
-       
-/*
-        // Display the content of the JSON list
-        for (JSONObject json : jsonList) {
-            System.out.println(json.toJSONString());
-        }
-    */
+    
     }
     
    
