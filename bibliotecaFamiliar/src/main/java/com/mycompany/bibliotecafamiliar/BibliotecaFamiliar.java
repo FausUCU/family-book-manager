911871eb822b2e21db61166b95c1040c2dd84b22
FausUCU <88690038+FausUCU@users.noreply.github.com>
2023-07-27 16:34:08 -0300

    GUI Practices
    practices/Test of GUI in Java

--- bibliotecaFamiliar/src/main/java/com/mycompany/bibliotecafamiliar/BibliotecaFamiliar.java
+++ bibliotecaFamiliar/src/main/java/com/mycompany/bibliotecafamiliar/BibliotecaFamiliar.java
@@ -13,7 +13,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -26,43 +25,7 @@ import java.net.URISyntaxException;
 public class BibliotecaFamiliar {
 
     public static void main(String[] args) throws IOException, ParseException {
-    String TestfilePath = "test.json";
-    JSONFileManager jsonFileManager = new JSONFileManager();
-    List<JSONObject> jsonList = jsonFileManager.readListOfJsonsFromFile(TestfilePath);
-    String first=jsonList.get(1).toJSONString();
-         System.out.println(first);
-        // Display the content of the JSON list
-        for (JSONObject json : jsonList) {
-            System.out.println(json.toJSONString());
-        }
-
-
-    /* 
-        String filePath = "libros.json";
-        String TestfilePath = "test.json";
-        JSONFileManager jsonFileManager = new JSONFileManager();
-        List<JSONObject> jsonList = jsonFileManager.readListOfJsonsFromFile(filePath);
-        TLibro testBook=new TLibro("testName", "testAutor", "testGener","testOwner",false);
-        JSONObject jsonBook=testBook.getJson();
-        jsonFileManager.addJsonFile(jsonBook, TestfilePath);
-      */ 
-
-        /* 
-        String first=jsonList.get(1).toJSONString();
-         System.out.println(first);
-        // Display the content of the JSON list
-        for (JSONObject json : jsonList) {
-            System.out.println(json.toJSONString());
-        }
-        TLibro testBook=new TLibro("testName", "testAutor", "testGener");
-        String title=testBook.getTitle();
-        String author=testBook.getAuthorList().get(0);
-        String genre=testBook.getGenre();
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(genre);
-
-        */
+    new GUI();
     
     }
     
